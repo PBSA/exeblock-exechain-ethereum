cb7c9bc5eaf66486c54311954d0128f9b95a816d
Alex Dulub <alex.dulub@designsters.com>
2017-11-03 11:00:19 +0300

    Changed ExtVM::create and ExtVM::call.

--- libethereum/ExtVM.h
+++ libethereum/ExtVM.h
@@ -66,10 +66,12 @@ public:
 	virtual size_t codeSizeAt(Address _a) override final;
 
 	/// Create a new contract.
-	virtual h160 create(u256 _endowment, u256& io_gas, bytesConstRef _code, OnOpFunc const& _onOp = {}) override final;
+	// virtual h160 create(u256 _endowment, u256& io_gas, bytesConstRef _code, OnOpFunc const& _onOp = {}) override final;
+	virtual h160 create(u256 _endowment, u256& io_gas, bytesConstRef _code, OnOpFunc const& _onOp = {});
 
 	/// Create a new message call. Leave _myAddressOverride as the default to use the present address as caller.
-	virtual bool call(CallParameters& _params) override final;
+	// virtual bool call(CallParameters& _params) override final;
+	virtual bool call(CallParameters& _params);
 
 	/// Read address's balance.
 	virtual u256 balance(Address _a) override final { return m_s.balance(_a); }
